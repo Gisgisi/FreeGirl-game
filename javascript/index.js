e170041451f71e08f1e4fb16071cee23a24ae68f
John Doe <atarod.shirin@gmail.com>
2022-10-17 16:45:16 +0200

    js and css modified

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,40 @@
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext("2d");
+
+
+const background = new Image()
+background.src = "../images/City.webp"
+
+const girl = new Image()
+girl.src = "../images/woman.png"
+
+const flower = new Image()
+flower.src = "../images/flower.png"
+
+const hair = new Image()
+hair.src = "../images/hair.png"
+
+const obs = new Image()
+obs.src = "../images/khamenei.png"
+
+
+bgX = 0;
+bgY = 0;
+girlX = 800
+girlY =  0
+
+function draw(){
+    ctx.drawImage(background, bgX, bgY, canvas.width, canvas.height);
+    ctx.drawImage(girl, girlX, girlY, 80, 80);
+    ctx.drawImage(flower, flowerX,flowerY, 40, 40);
+    ctx.drawImage(hair,hairX,hairY,40,40)
+} 
+
+function startGame(){
+    canvas.style.display = block;
+}
+
+
+    document.getElementById("btn-start").addEventListener('click',
+    startGame());
 
