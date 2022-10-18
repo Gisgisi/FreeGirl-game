2d05dbcdf873998052512dd01fa8e64f2f84f32f
John Doe <atarod.shirin@gmail.com>
2022-10-18 12:54:12 +0200

    start modified

--- javascript/index.js
+++ javascript/index.js
@@ -17,23 +17,34 @@ hair.src = "../images/hair.png"
 const obs = new Image()
 obs.src = "../images/khamenei.png"
 
+const gamePlay = document.querySelector('#game-play');
 
 bgX = 0;
 bgY = 0;
-girlX = 800
-girlY = 0
+girlX = 10;
+girlY = 400;
+hairX = 100;
+hairY = 450;
+flowerX = 300;
+flowerY = 400;
+obsX = 250;
+obsY = 400;
+
 
 function draw() {
     ctx.drawImage(background, bgX, bgY, canvas.width, canvas.height);
     ctx.drawImage(girl, girlX, girlY, 80, 80);
     ctx.drawImage(flower, flowerX, flowerY, 40, 40);
     ctx.drawImage(hair, hairX, hairY, 40, 40);
-    ctx.drawImage(obs, obsX, obsY, 40, 40);
+    ctx.drawImage(obs, obsX, obsY, 35, 65);
+
 }
+
 
 function startGame() {
     gameIntro.style.display = "none";
-    canvas.style.display = "block";
+    gamePlay.style.display = "block";;
+    draw()
 }
 
 window.onload = () => {
